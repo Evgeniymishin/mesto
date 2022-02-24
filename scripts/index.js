c21b6ba52db4cae5010bb3530b99de4735483010
Evgeniy Mishin <dubreak@yandex.ru>
2022-02-24 18:31:49 +0300

    feat: Добавлен скрипт работы с попапом

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+let root = document.querySelector('.root');
+let editButton = root.querySelector('.profile-info__edit-button');
+let popup = root.querySelector('.popup');
+let addButton = root.querySelector('.profile__add-button');
+let closeButton = root.querySelector('.popup__close-button');
+let profileTitle = root.querySelector('.popup__input_type_title');
+let profileSubtitle = root.querySelector('.popup__input_type_subtitle');
+let form = root.querySelector('.popup__container');
+let profileName = root.querySelector('.profile-info__title');
+let profileInfo = root.querySelector('.profile-info__subtitle');
+
+editButton.addEventListener('click', runEditForm);
+closeButton.addEventListener('click', closeForm);
+form.addEventListener('submit', formSubmitHandler);
+
+function runEditForm() {
+  popup.classList.add('popup_opened');
+  let profileName = root.querySelector('.profile-info__title');
+  profileTitle.value = profileName.textContent;
+  let profileInfo = root.querySelector('.profile-info__subtitle');
+  profileSubtitle.value = profileInfo.textContent;
+}
+
+function closeForm() {
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = profileTitle.value; 
+  profileInfo.textContent = profileSubtitle.value;
+  closeForm();
+}
