1a7d902afb96dd23fcecc5a8072754ff67899c7a
Evgeniy Mishin <dubreak@yandex.ru>
2022-02-26 00:50:37 +0300

    Исправлены ошибки по результатам ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,21 +3,20 @@ let editButton = root.querySelector('.profile-info__edit-button');
 let popup = root.querySelector('.popup');
 let closeButton = root.querySelector('.popup__close-button');
 let profileTitle = root.querySelector('.popup__input_type_title');
-let profileSubtitle = root.querySelector('.popup__input_type_subtitle');
+let profileText = root.querySelector('.popup__input_type_text');
 let form = root.querySelector('.popup__container');
 let profileName = root.querySelector('.profile-info__title');
-let profileInfo = root.querySelector('.profile-info__subtitle');
+let profileInfo = root.querySelector('.profile-info__text');
 
 editButton.addEventListener('click', runEditForm);
 closeButton.addEventListener('click', closeForm);
 form.addEventListener('submit', formSubmitHandler);
+popup.addEventListener('click', closeFormByArea);
 
 function runEditForm() {
   popup.classList.add('popup_opened');
-  let profileName = root.querySelector('.profile-info__title');
   profileTitle.value = profileName.textContent;
-  let profileInfo = root.querySelector('.profile-info__subtitle');
-  profileSubtitle.value = profileInfo.textContent;
+  profileText.value = profileInfo.textContent;
 }
 
 function closeForm() {
@@ -27,6 +26,12 @@ function closeForm() {
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = profileTitle.value; 
-  profileInfo.textContent = profileSubtitle.value;
+  profileInfo.textContent = profileText.value;
   closeForm();
+}
+
+function closeFormByArea(evt) {
+  if (evt.target === evt.currentTarget) {
+    closeForm();
+  }
 }
